8fbe9aea1218f3b605d092d58d854ed40f7b6842
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2022-05-03 21:31:18 +0200

    Initial commit

---
+++ src/main/java/net/quickwrite/miniminigames/MiniMinigames.java
@@ -0,0 +1,17 @@
+package net.quickwrite.miniminigames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniMinigames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
