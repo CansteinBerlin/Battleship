aae6c4bc7f22f4fdbe8690363bbf6520984acbf8
Hasenzahn1 <motzer10@gmx.de>
2022-05-24 15:43:03 +0200

    Dynamical importing.
    Added the Reflections Dependency to register all SubCommand in a specific package so you can faster develop.

--- src/main/java/net/quickwrite/miniminigames/commands/DebugCommand.java
+++ src/main/java/net/quickwrite/miniminigames/commands/DebugCommand.java
@@ -1,25 +1,24 @@
 package net.quickwrite.miniminigames.commands;
 
+import net.quickwrite.miniminigames.MiniMinigames;
 import net.quickwrite.miniminigames.commands.debug.*;
 import net.quickwrite.miniminigames.commandsystem.BaseCommand;
+import net.quickwrite.miniminigames.commandsystem.SubCommand;
+import net.quickwrite.miniminigames.util.ReflectionUtil;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class DebugCommand extends BaseCommand {
 
     public DebugCommand() {
         super("mdebug", null);
 
-        addSubCommand(new DebugSaveShipConfigCommand(this));
-        addSubCommand(new DebugAddShipCommand(this));
-        addSubCommand(new DebugSaveItemConfigCommand(this));
-        addSubCommand(new DebugAddItemCommand(this));
-        addSubCommand(new DebugAddBlockCommand(this));
-        addSubCommand(new DebugSaveBlockConfigCommand(this));
-        addSubCommand(new DebugEnableSelectionCommand(this));
-        addSubCommand(new DebugAddVerticalDisplayCommand(this));
-        addSubCommand(new DebugAddPlayerToDisplayCommand(this));
-        addSubCommand(new DebugRemovePlayerFromDisplayCommand(this));
-        addSubCommand(new DebugAddRandomBlockToDisplayCommand(this));
-        addSubCommand(new DebugClearDisplayCommand(this));
-        addSubCommand(new DebugAddHorizontalDisplayCommand(this));
+        for(Class<? extends SubCommand> command : ReflectionUtil.getAllClasses("net.quickwrite.miniminigames.commands.debug", SubCommand.class)){
+            try {
+                addSubCommand(command.getConstructor(BaseCommand.class).newInstance(this));
+            }catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e){
+                MiniMinigames.logger.severe("§cCould not register Subcommand for " + name);
+            }
+        }
     }
 }
