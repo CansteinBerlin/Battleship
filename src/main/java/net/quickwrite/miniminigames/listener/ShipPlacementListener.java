d9f108b34a62575955f5c7d9e3aa658814d414ca
Hasenzahn1 <motzer10@gmx.de>
2022-07-30 14:09:16 +0200

    QolI 4 Dropping the ship now changes the direction.
    Why did i have to do this.
    NO PAPER!
    DROPPING AN ITEM IS NOT THE SAME AS LEFT CLICKING AIR!

--- src/main/java/net/quickwrite/miniminigames/listener/ShipPlacementListener.java
+++ src/main/java/net/quickwrite/miniminigames/listener/ShipPlacementListener.java
@@ -4,15 +4,25 @@ import net.quickwrite.miniminigames.MiniMinigames;
 import net.quickwrite.miniminigames.game.Game;
 import net.quickwrite.miniminigames.game.gamestate.GameStateManager;
 import net.quickwrite.miniminigames.ships.Ship;
+import net.quickwrite.miniminigames.util.DebugMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
 
 public class ShipPlacementListener implements Listener {
+
+    private final ArrayList<Player> droppedOnTick = new ArrayList<>();
 
     @EventHandler
     public void onSneak(PlayerToggleSneakEvent event){
@@ -25,8 +35,29 @@ public class ShipPlacementListener implements Listener {
         game.toggleShipDirection(p);
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDrop(PlayerDropItemEvent event){
+        Player p = event.getPlayer();
+        Game game = MiniMinigames.getInstance().getGameManager().getGame(p);
+        if(game == null) return;
+        if(!game.getCurrentGameState().equals(GameStateManager.GameState.PLACING_SHIPS)) return;
+        game.toggleShipDirection(p);
+        event.setCancelled(true);
+
+        droppedOnTick.add(p);
+        new BukkitRunnable(){
+
+            @Override
+            public void run() {
+                droppedOnTick.remove(p);
+            }
+        }.runTaskLater(MiniMinigames.getInstance(), 2L);
+    }
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
+        if(droppedOnTick.contains(event.getPlayer())) return;
+        if(event.getAction() != Action.RIGHT_CLICK_BLOCK && event.getAction() != Action.LEFT_CLICK_BLOCK && event.getAction() != Action.LEFT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_AIR) return;
         if(event.getItem() == null) return;
         if(event.getItem().getType().equals(Material.AIR)) return;
         if(event.getItem().getItemMeta() == null) return;
